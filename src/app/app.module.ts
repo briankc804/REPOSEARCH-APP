cf0034ff67a17a573911e32be040a1edb6442e53
briankc804 <briankc804@gmail.com>
2022-04-18 01:50:44 +0300

    created a repo form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReposearchComponent } from './reposearch/reposearch.component';
+import { RepoFormComponent } from './repo-form/repo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReposearchComponent
+    ReposearchComponent,
+    RepoFormComponent
   ],
   imports: [
     BrowserModule,
